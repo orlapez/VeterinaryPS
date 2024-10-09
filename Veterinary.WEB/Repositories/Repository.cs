c7c0e0a4e7f4fc723593b7427d3325560a7d3b75
orlapez <orlapez@gmail.com>
2024-10-09 07:54:42 -0500

    Patrón Repository

---
+++ Veterinary.WEB/Repositories/Repository.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Veterinary.WEB.Repositories
+{
+    public class Repository : IRepository
+    {
+        public Task<HttpResponseWrapper<T>> GetAsync<T>(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseWrapper<TResponse>> PostAsync<T, TResponse>(string url, T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
